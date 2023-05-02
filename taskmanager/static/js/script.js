a9175284b047d55365e5059bec3076e7037f0138
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-05-02 15:23:04 +0100

    add 'Add Task' functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,18 @@
 // initialization from Materialize https://materializecss.com/navbar.html
 
 document.addEventListener('DOMContentLoaded', function () {
+    //sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+	M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+	M.FormSelect.init(selects);
 });
